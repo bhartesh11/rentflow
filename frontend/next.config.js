0bf695e7a374c069c2b2985f023ebd77fe5e5b5e
Kajal <kajal.kumari@ficode.com>
2026-08-02 21:10:39 +0530

    ncx config file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL ||
+      "https://YOUR_BACKEND_URL/api",
   },
 };
 
